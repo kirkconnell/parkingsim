8461aaeed82cd8b1c5450df6bc68e760f176a74b
kirkconnell <carlos.kirkconnell@gmail.com>
2010-04-04 15:24:50 -0500

    adding building specs

---
+++ spec/parkingsim/building_spec.rb
@@ -0,0 +1,12 @@
+require File.join(File.dirname(__FILE__), "../spec_helper.rb")
+
+describe Building do
+  context "when configuring" do
+    it "should accept dimensions" do
+      building.dimensions :floors => 3, :rows => 10, :spots => 10
+      building.to_ar.length.should == 3
+      building.to_ar[0].length.should == 10
+      building.to_ar[0][0].length.should == 10
+    end
+  end
+end
