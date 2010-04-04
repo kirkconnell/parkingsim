ffc06d0b28c4817adb88b796a5344cee060fbd21
kirkconnell <carlos.kirkconnell@gmail.com>
2010-04-04 14:48:13 -0500

    initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  parkingsim
+//
+//  Created by Kirkconnell on 4/4/10.
+//  Copyright High Notes 2010. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
