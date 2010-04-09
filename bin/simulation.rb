2b69fd0254e4d5e3092628ef0a82e09a23a6590b
kirkconnell <carlos.kirkconnell@gmail.com>
2010-04-09 16:42:22 -0500

    fixing bugs in the run of the simulaiton

--- bin/simulation.rb
+++ bin/simulation.rb
@@ -6,6 +6,7 @@ Car.display_delegate { |message| puts message }
 
 building = Building.new :floors => 3, :rows => 3, :spots => 3
 car = Car.new(building)
+CarFactory.instance.building = building
 
 car.decide_next_action!
 EventQueue.add_event(car, car.next_action)
