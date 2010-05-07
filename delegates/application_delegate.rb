1c211dacacdb079278dd025923c9254b19770ac9
kirkconnell <carlos.kirkconnell@gmail.com>
2010-05-07 17:38:54 -0500

    making the event queue table work

--- delegates/application_delegate.rb
+++ delegates/application_delegate.rb
@@ -4,49 +4,56 @@
 # Created by Kirkconnell on 4/19/10.
 # Copyright 2010 High Notes. All rights reserved.
 require 'message_log_delegate'
+require 'event_queue_delegate'
 
 class ApplicationDelegate
 
-  attr_accessor :play_button, :event_log_table
+  attr_accessor :play_button, :event_log_table, :event_queue_table
   attr_accessor :total_label, :running_label, :parked_label
   attr_reader :logged_messages
   
   def initialize
-	@logged_messages = []
-	@running = false
+  	@logged_messages = []
+  	@running = false
   end
 
   def applicationDidFinishLaunching(notification)
-	NSLog "Loading Simulation..."
+  	NSLog "Loading Simulation..."
 
-	@building = Building.new :floors => 3, :rows => 3, :spots => 3
-	CarFactory.instance.building = @building
-	Car.display_delegate { |msg| @logged_messages << [Time.now, msg] }
+  	@building = Building.new :floors => 3, :rows => 3, :spots => 3
+  	CarFactory.instance.building = @building
+  	Car.display_delegate { |msg| @logged_messages << [Time.now, msg] }
 	
-	NSLog "Simulation Ready."
+  	NSLog "Simulation Ready."
   end
   
   def start_simulation(sender)
-	unless @running
-	  @running = true
-	  NSLog "Starting Simulation..."
-	  @timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, 
-				selector: "tick", userInfo:nil, repeats:true)
-	  play_button.title = "Stop"
-	else
-	  @running = false
-	  @timer.invalidate
-	  NSLog "Stopping Simulation."
-	  play_button.title = "Run"
-	end
+  	unless @running
+  	  @running = true
+  	  NSLog "Starting Simulation..."
+  	  @timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, 
+  				selector: "tick", userInfo:nil, repeats:true)
+  	  play_button.title = "Stop"
+  	else
+  	  @running = false
+  	  @timer.invalidate
+  	  NSLog "Stopping Simulation."
+  	  play_button.title = "Run"
+  	end
   end
   
   def tick
-	Simulation.tick
-	event_log_table.reloadData
-	event_log_table.scrollRowToVisible(logged_messages.length - 1)
-	total_label.stringValue = Simulation.cars.length.to_s
-	running_label.stringValue = (Simulation.cars.inject(0) { |sum, c| sum + (c.on? ? 1 : 0) }).to_s
-	parked_label.stringValue = (Simulation.cars.inject(0) { |sum, c| sum + (c.off? ? 1 : 0) }).to_s
+  	Simulation.tick
+  	refresh_ui
+  end
+  
+  def refresh_ui
+    event_log_table.reloadData
+  	event_log_table.scrollRowToVisible(logged_messages.length - 1)
+  	event_queue_table.reloadData
+  	
+  	total_label.stringValue = Simulation.cars.length.to_s
+  	running_label.stringValue = (Simulation.cars.inject(0) { |sum, c| sum + (c.on? ? 1 : 0) }).to_s
+  	parked_label.stringValue = (Simulation.cars.inject(0) { |sum, c| sum + (c.off? ? 1 : 0) }).to_s
   end
 end
--- delegates/message_log_delegate.rb
+++ delegates/message_log_delegate.rb
@@ -8,17 +8,17 @@ class MessageLogDelegate
   attr_accessor :parent
   
   def numberOfRowsInTableView(tableView)
-	parent.logged_messages.length
+	  parent.logged_messages.length
   end
   
   def tableView(tableView, objectValueForTableColumn:column, row:row)
-	if row < parent.logged_messages.length
-	  if column.identifier == "time"
-		parent.logged_messages[row].first.strftime("%I:%M:%S %p")
-	  else
-		parent.logged_messages[row].last
-	  end
-	end
+  	if row < parent.logged_messages.length
+  	  if column.identifier == "time"
+  		  parent.logged_messages[row].first.strftime("%I:%M:%S %p")
+  	  else
+  		  parent.logged_messages[row].last
+  	  end
+  	end
   end
 end
 
